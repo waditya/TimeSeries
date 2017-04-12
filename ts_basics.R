513f649d6bed54d8ac8d216f135c4620a853659d
waditya <waditya02@gmail.com>
2017-04-12 22:29:01 +0530

    TimeSeries_Basics_01.R
    TimeSeries_Basics_01

---
+++ ts_basics.R
@@ -0,0 +1,12 @@
+# Print the Nile dataset
+print(Nile)
+
+# List the number of observations in the Nile dataset
+
+length(Nile)
+# Display the first 10 elements of the Nile dataset
+head(Nile, n = 10)
+
+# Display the last 12 elements of the Nile dataset
+
+tail(Nile, n = 12)
