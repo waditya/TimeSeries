896f362c410440df911b98b41ba16d19ae472fee
waditya <waditya02@gmail.com>
2017-04-12 22:35:23 +0530

    Update timeseries_basics_01.R

--- timeseries_basics_01.R
+++ timeseries_basics_01.R
@@ -10,3 +10,13 @@ head(Nile, n = 10)
 # Display the last 12 elements of the Nile dataset
 
 tail(Nile, n = 12)
+
+# Plot the Nile data
+plot(Nile)
+
+# Plot the Nile data with xlab and ylab arguments
+plot(Nile, xlab = "Year", ylab = "River Volume (1e9 m^{3})")
+
+# Plot the Nile data with xlab, ylab, main, and type arguments
+plot(Nile, xlab = "Year", ylab = "River Volume (1e9 m^{3})", main = "Annual River Nile Volume at Aswan, 1871-1970", type ="b")
+
