5fb523cfcd2885f46d52c2f1475ab8dccd41fc7a
waditya <waditya02@gmail.com>
2017-04-15 23:26:41 +0530

    Update Ch_2_Predicting_the_future.R

--- Ch_2_Predicting_the_future.R
+++ Ch_2_Predicting_the_future.R
@@ -16,3 +16,16 @@ ts.plot(dz)
 length(z)
 length(dz)
 
+# Generate a diff of x with lag = 4. Save this to dx
+dx <- diff(x, lag = 4)
+  
+# Plot dx
+ts.plot(dx)
+
+# View the length of x and dx, respectively 
+length(x)
+length(dx)
+
+##Great work! Once again differencing allows you to remove the longer-term time trend - in this case, seasonal volatility - and 
+##focus on the change from one period to another.
+
