97d8bb6f180e0f7e70b26b75b28b18692d93e5dd
waditya <waditya02@gmail.com>
2017-04-15 23:10:48 +0530

    Update timeseries_basics_01.R

--- timeseries_basics_01.R
+++ timeseries_basics_01.R
@@ -1,3 +1,5 @@
+# Chapter -1 Exploratory time series data analysis
+
 # Print the Nile dataset
 print(Nile)
 
@@ -81,4 +83,20 @@ is.ts(Nile)
 # Check whether AirPassengers is a ts object
 is.ts(AirPassengers)
 
+# Check whether eu_stocks is a ts object
+is.ts(eu_stocks)
+
+# View the start, end, and frequency of eu_stocks
+start(eu_stocks)
+end(eu_stocks)
+frequency(eu_stocks)
+
+# Generate a simple plot of eu_stocks
+plot(eu_stocks)
+
+# Use ts.plot with eu_stocks
+ts.plot(eu_stocks, col = 1:4, xlab = "Year", ylab = "Index Value", main = "Major European Stock Indices, 1991-1998")
+
+# Add a legend to your ts.plot
+legend("topleft", colnames(eu_stocks), lty = 1, col = 1:4, bty = "n")
 
