cf350c1a758faf128f5e08847a2f5c8c8888936c
waditya <waditya02@gmail.com>
2017-04-15 23:23:58 +0530

    Update Ch_2_Predicting_the_future.R

--- Ch_2_Predicting_the_future.R
+++ Ch_2_Predicting_the_future.R
@@ -4,3 +4,15 @@ linear_growth <- log(rapid_growth)
   
 # Plot linear_growth using ts.plot()
  ts.plot(linear_growth)
+
+# Generate the first difference of z
+dz <- diff(z)
+  
+# Plot dz
+
+ts.plot(dz)
+
+# View the length of z and dz, respectively
+length(z)
+length(dz)
+
