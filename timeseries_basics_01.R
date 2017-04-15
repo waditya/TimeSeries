68d97cea76fadc0e99f7462bb51f44f81e5a2312
waditya <waditya02@gmail.com>
2017-04-15 23:00:21 +0530

    Update timeseries_basics_01.R

--- timeseries_basics_01.R
+++ timeseries_basics_01.R
@@ -42,3 +42,21 @@ time(AirPassengers)
 deltat(AirPassengers)
 frequency(AirPassengers)
 cycle(AirPassengers)
+
+# Plot the AirPassengers data
+plot(AirPassengers)
+
+# Compute the mean of AirPassengers
+mean(AirPassengers, na.rm = TRUE)
+
+# Impute mean values to NA in AirPassengers
+AirPassengers[85:96] <- mean(AirPassengers, na.rm = TRUE)
+
+# Generate another plot of AirPassengers
+plot(AirPassengers)
+
+# Add the complete AirPassengers data to your plot
+rm(AirPassengers)
+points(AirPassengers, type = "l", col = 2, lty = 3)
+plot(AirPassengers)
+
