efed339ef2708fcd9d233d4a934587ade230f8d6
waditya <waditya02@gmail.com>
2017-04-16 18:39:22 +0530

    Update Ch_2_Predicting_the_future.R

--- Ch_2_Predicting_the_future.R
+++ Ch_2_Predicting_the_future.R
@@ -29,3 +29,31 @@ length(dx)
 ##Great work! Once again differencing allows you to remove the longer-term time trend - in this case, seasonal volatility - and 
 ##focus on the change from one period to another.
 
+# The white noise (WN) model is a basic time series model. It is also a basis for the more elaborate models we will consider. 
+
+# We will focus on the simplest form of WN, independent and identically distributed data.
+
+# The arima.sim() function can be used to simulate data from a variety of time series models. 
+
+# ARIMA is an abbreviation for the autoregressive integrated moving average class of models we will consider throughout this course.
+
+# An ARIMA(p, d, q) model has three parts, the autoregressive order p, the order of integration (or differencing) d, and the 
+
+# moving average order q. We will detail each of these parts soon, but for now we note that the ARIMA(0, 0, 0) model, 
+
+# i.e., with all of these components zero, is simply the WN model.
+
+# In this exercise, you will practice simulating a basic WN model.
+
+# Simulate a WN model with list(order = c(0, 0, 0))
+white_noise <- arima.sim(model = list(order = c(0, 0, 0)), n = 100)
+
+# Plot your white_noise data
+ts.plot(white_noise)
+
+# Simulate from the WN model with: mean = 100, sd = 10
+white_noise_2 <- arima.sim(model = list(order = c(0, 0, 0)), n = 100, mean = 100, sd = 10)
+
+# Plot your white_noise_2 data
+ts.plot(white_noise_2)
+
