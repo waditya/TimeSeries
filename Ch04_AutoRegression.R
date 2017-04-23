1c753b90003e6cf3408c6d41ddbdc34a6e6065e5
waditya <waditya02@gmail.com>
2017-04-23 12:53:37 +0530

    Update Ch04_AutoRegression.R

--- Ch04_AutoRegression.R
+++ Ch04_AutoRegression.R
@@ -11,3 +11,15 @@
 
 #In this exercise, you will use this command to simulate and plot three different AR models 
 #with slope parameters equal to 0.5, 0.9, and -0.75, respectively.
+
+# Simulate an AR model with 0.5 slope
+x <- arima.sim(model = list(ar = 0.5), n = 100)
+
+# Simulate an AR model with 0.9 slope
+y <- arima.sim(model = list(ar = 0.9) ,n = 100)
+
+# Simulate an AR model with -0.75 slope
+z <-  arima.sim(model = list(ar = -0.75), n= 100)
+
+# Plot your simulated data
+plot.ts(cbind(x, y, z))
