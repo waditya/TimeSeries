f4a54b18b1dbeff06292f424c7fc0f05a9ae15d3
waditya <waditya02@gmail.com>
2017-04-23 16:15:06 +0530

    Update Ch05_Simple_Moving_Average.R

--- Ch05_Simple_Moving_Average.R
+++ Ch05_Simple_Moving_Average.R
@@ -69,3 +69,65 @@ ts.plot(Nile)
 MA_fit <- Nile - resid(MA)
 points(MA_fit, type = "l", col = 2, lty = 2)
 
+#Simple forecasts from an estimated MA model
+
+# # Now that you've estimated a MA model with your Nile data, the next step is to do some simple forecasting with your model. 
+# As with other types of models, you can use the predict() function to make simple forecasts from your estimated MA model. 
+# Recall that the $pred value is the forecast, while the $se value is a standard error for that forecast, 
+# each of which is based on the fitted MA model.
+
+# Once again, to make predictions for several periods beyond the last observation you can use the n.ahead = h argument 
+# in your call to predict(). The forecasts are made recursively from 1 to h-steps ahead from the end of the observed time series. 
+# However, note that except for the 1-step forecast, all forecasts from the MA model are equal to the estimated mean (intercept).
+
+# In this exercise, you'll use the MA model derived from your Nile data to make simple forecasts about future River Nile flow levels. 
+# Your MA model from the previous exercise is available in your environment.
+
+# Make a 1-step forecast based on MA
+predict_MA <- predict(MA)
+
+# Obtain the 1-step forecast using $pred[1]
+predict_MA$pred[1] 
+
+# Make a 1-step through 10-step forecast based on MA
+predict(MA, n.ahead = 10)
+
+# Plot the Nile series plus the forecast and 95% prediction intervals
+ts.plot(Nile, xlim = c(1871, 1980))
+MA_forecasts <- predict(MA, n.ahead = 10)$pred
+MA_forecast_se <- predict(MA, n.ahead = 10)$se
+points(MA_forecasts, type = "l", col = 2)
+points(MA_forecasts - 2*MA_forecast_se, type = "l", col = 2, lty = 2)
+points(MA_forecasts + 2*MA_forecast_se, type = "l", col = 2, lty = 2)
+
+
+AR vs MA models
+
+# As you've seen, autoregressive (AR) and simple moving average (MA) are two useful approaches to modeling time series. 
+# But how can you determine whether an AR or MA model is more appropriate in practice?
+
+# To determine model fit, you can measure the Akaike information criterion (AIC) and Bayesian information criterian (BIC) for each model. 
+# While the math underlying the AIC and BIC is beyond the scope of this course, for your purposes the main idea is these these indicators 
+# penalize models with more estimated parameters, to avoid overfitting, and smaller values are preferred. All factors being equal, a model 
+# that produces a lower AIC or BIC than another model is considered a better fit.
+
+# To estimate these indicators, you can use the AIC() and BIC() commands, both of which require a single argument to specify the model in question.
+
+# In this exercise, you'll return to the Nile data and the AR and MA models you fitted to this data. These models and their predictions for the 
+# 1970s (AR_fit) and (MA_fit) are depicted in the plot on the right.
+
+
+# Find correlation between AR_fit and MA_fit
+cor(AR_fit, MA_fit)
+
+# Find AIC of AR
+AIC(AR)
+
+# Find AIC of MA
+AIC(MA)
+
+# Find BIC of AR
+BIC(AR)
+
+# Find BIC of MA
+BIC(MA)
