7965d630e8bb05f6482f8dfd69c4c0ac7802d625
waditya <waditya02@gmail.com>
2017-04-23 12:50:55 +0530

    Update Ch04_AutoRegression.R

--- Ch04_AutoRegression.R
+++ Ch04_AutoRegression.R
@@ -1,1 +1,13 @@
+#Simulate the autoregressive model
 
+#The autoregressive (AR) model is arguably the most widely used time series model. 
+#It shares the very familiar interpretation of a simple linear regression, but here 
+#each observation is regressed on the previous observation. The AR model also includes 
+#the white noise (WN) and random walk (RW) models examined in earlier chapters as special cases.
+
+#The versatile arima.sim() function used in previous chapters can also be used to simulate data from 
+#an AR model by setting the model argument equal to list(ar = phi) , in which phi is a slope parameter 
+#from the interval (-1, 1). We also need to specify a series length n.
+
+#In this exercise, you will use this command to simulate and plot three different AR models 
+#with slope parameters equal to 0.5, 0.9, and -0.75, respectively.
