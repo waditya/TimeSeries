2a886c5456cf403c97898e593f73a187d554f561
waditya <waditya02@gmail.com>
2017-04-23 16:07:03 +0530

    Update Ch05_Simple_Moving_Average.R

--- Ch05_Simple_Moving_Average.R
+++ Ch05_Simple_Moving_Average.R
@@ -36,3 +36,36 @@ acf(y)
 
 # Calculate ACF for z
 acf(z)
+
+##---
+#Estimate the simple moving average model
+
+#Now that you've simulated some MA models and calculated the ACF from these models, your next step is to 
+# fit the simple moving average (MA) model to some data using the arima() command. For a given time series 
+# x we can fit the simple moving average (MA) model using arima(..., order = c(0, 0, 1)). Note for reference 
+# that an MA model is an ARIMA(0, 0, 1) model.
+
+# In this exercise, you'll practice using a preloaded time series (x, shown in the plot on the right) as well 
+# as the Nile dataset used in earlier chapters.
+
+# Fit the MA model to x
+arima(x, order = c(0, 0, 1))
+
+# Paste the slope (ma1) estimate below
+0.7928
+
+# Paste the slope mean (intercept) estimate below
+0.1589
+
+# Paste the innovation variance (sigma^2) estimate below
+0.9576
+
+# Fit the MA model to Nile
+MA <- arima(Nile, order = c(0, 0, 1))
+print(MA)
+
+# Plot Nile and MA_fit 
+ts.plot(Nile)
+MA_fit <- Nile - resid(MA)
+points(MA_fit, type = "l", col = 2, lty = 2)
+
