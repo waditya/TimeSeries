906f6407130f0d2208cea3e65af0286de8f90cfd
waditya <waditya02@gmail.com>
2017-06-08 06:26:27 +0530

    Create Ch04_Seasonal_ARIMA.R

--- Ch04_Seasonal_ARIMA.R
+++ Ch04_Seasonal_ARIMA.R
@@ -11,3 +11,9 @@
 # Instructions
 # Plot the sample ACF and PACF of the generated data to lag 60 (max.lag = 60) and compare them to the actual values.
 # Fit the model to generated data (x) using sarima(). As in the previous exercise, be sure to specify the additional seasonal arguments in your sarima() command.
+
+# Plot sample P/ACF pair to lag 60 and compare to actual
+acf2(x, max.lag = 60)
+
+# Fit the seasonal model to x
+sarima(x, p = 0, d = 0, q = 1, P = 0 , D = 0, Q = 1, S = 12 )
