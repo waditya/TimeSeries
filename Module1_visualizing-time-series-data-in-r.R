51637906dd7ba3e68593110e8733a64300e92d80
waditya <waditya02@gmail.com>
2017-06-29 17:37:45 -0500

    Update Module1_visualizing-time-series-data-in-r.R

--- Module1_visualizing-time-series-data-in-r.R
+++ Module1_visualizing-time-series-data-in-r.R
@@ -61,4 +61,23 @@ plot(data$microsoft, main = "microsoft", sub = "Daily closing price since 2015",
 # Change the color of the line to red
 lines(data$microsoft, main = "microsoft", sub = "Daily closing price since 2015", type = "h",col = "red")
 
+# Control graphic parameters
 
+# We have seen how to change the parameters of a chart but it is also possible to tailor the window layout. 
+
+# You might want to draw 2 or more charts on the same graphical window, change the size of the margin or the character. 
+
+# All this can be acheived very easily using the par() function. You are provided with the same dataset as before.
+
+# To setup the graphical window for 2 charts, the code is:
+
+# par(mfrow = c(2, 1))
+# Where 2 is the number of rows and 1 is the number of column to setup. After that, just make two calls to plot() to add the series.
+
+# Instructions
+
+# Put the first 2 series of the dataset onto the same graphical window in order, including the title for each of them
+
+# Reduce margin size (mex) to 60% of its normal size and replot
+
+# Reduce margin size and character size (cex) to 80% of their normal size and replot
